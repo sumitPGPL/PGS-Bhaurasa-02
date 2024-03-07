5954fde4234d12b1c6b9327b5c1d34cc82ba4e40
sumitPGPL <sumitpgpl@gmail.com>
2024-03-07 13:29:10 +0530

    first commit

---
+++ src/app/admin/events/page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import EventParentForm from '@/components/Event/EventParentForm'
+
+export default function page() {
+  return (
+    <div><EventParentForm/></div>
+  )
+}
